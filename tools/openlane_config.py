70913d4956d03fd8ad71bb00f797ff3b1e2a51b7
Yin Wu <yinwu.life@gmail.com>
2024-02-23 10:59:49 +0100

    merge

--- tools/openlane_config.py
+++ tools/openlane_config.py
@@ -7,12 +7,13 @@ from loader.bev_road.openlane_data import OpenLane_dataset_with_offset,OpenLane_
 from models.model.single_camera_bev import BEV_LaneDet
 
 ''' data split '''
-train_gt_paths = '/root/workspace/bev_lane_det/dataset/openlane/lane3d_300/training'
-train_image_paths = '/root/workspace/bev_lane_det/dataset/openlane/images_training'
-val_gt_paths = '/root/workspace/bev_lane_det/dataset/openlane/lane3d_300/validation'
-val_image_paths = '/root/workspace/bev_lane_det/dataset/openlane/images_validation'
+root_path = '/home/yinwu/data/lane_detection/openlane'
+train_gt_paths = root_path + '/lane3d_1000/training'
+train_image_paths = root_path + '/images_training'
+val_gt_paths = root_path + '/lane3d_1000/validation'
+val_image_paths = root_path + '/images_validation'
 
-model_save_path = "/root/workspace/bev_lane_det/output"
+model_save_path = "./output/"
 
 input_shape = (576,1024)
 output_2d_shape = (144,256)
@@ -24,8 +25,8 @@ meter_per_pixel = 0.5 # grid size
 bev_shape = (int((x_range[1] - x_range[0]) / meter_per_pixel),int((y_range[1] - y_range[0]) / meter_per_pixel))
 
 loader_args = dict(
-    batch_size=24,
-    num_workers=18,
+    batch_size=6,  # 24 for 24GB GPU, 12 for 12GB GPU, 8=6 for 8GB GPU
+    num_workers=12,
     shuffle=True
 )
 
